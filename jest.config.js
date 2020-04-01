ce126d48a43904a8ef0409af93d82db0a305aafa
Jason Willmore <jason_m_willmore@homedepot.com>
2020-04-01 16:13:41 -0700

    tests run!

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,15 @@ module.exports = {
     "collectCoverageFrom": [
       "src/**/*.{js,jsx,ts,tsx}"
     ],
+    "setupFilesAfterEnv": [
+        '<rootDir>/testSetup.js',
+    ],
     "testResultsProcessor": "jest-sonar-reporter",
     "moduleNameMapper": {
       "\\.(css|scss)$": "identity-obj-proxy"
     },
     "transform": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js" ,
       "^.+\\.js$": "babel-jest"
     },
     "moduleFileExtensions": [
